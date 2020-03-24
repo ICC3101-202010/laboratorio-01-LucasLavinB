ab7e66345195faed3a6f4cedc32f0b3e76cfce18
LucasLavin <ljlavin@miuandes.cl>
2020-03-24 16:39:27 -0300

    Laboratorio2

---
+++ Laboratorio1-LucasLavín/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Laboratorio1_LucasLavín
+{
+    public class Person
+    {
+        //Atributos
+        private string nombre;
+        private string apellido;
+        private string lanzar;
+        private int randomNumber;
+        public Person(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public string GetApellido()
+        {
+            return apellido;
+        }
+        public void Lanzar(string lanzar, int randomNumber)
+        {
+            int randomNumber = new Random().Next(0, 3);
+            if (randomNumber == 0)
+            {
+                lanzar = "piedra";
+                this.lanzar = lanzar;
+                return lanzar;
+            }
+            else if (randomNumber == 1)
+            {
+                lanzar = "papel";
+                this.lanzar = lanzar;
+                return lanzar;
+            }
+            else if (randomNumber == 2)
+            {
+                lanzar = "tijera";
+                this.lanzar = lanzar;
+                return lanzar;
+            }
+        }
+    }
+	
+}
--- Laboratorio1-LucasLavín/Program.cs
+++ Laboratorio1-LucasLavín/Program.cs
@@ -2,57 +2,17 @@
 
 namespace Laboratorio1_LucasLavín
 {
-    class Program
+    class MainClass
     {
-        public class Persona
-        {
-            private string nombre;
-            private string apellido;
-            private int lanzar;
-            private string v1;
-            private string v2;
-
-            public Persona(string nombre, string apellido, int lanzar)
-            {
-                this.nombre = nombre;
-                this.apellido = apellido;
-                this.lanzar = lanzar;
-
-            }
-
-            public Persona(string v1, string v2)
-            {
-                this.v1 = v1;
-                this.v2 = v2;
-            }
-
-            public int Lanzar(int randomNumber)
-            {
-
-                return lanzar;
-            }
-            
-            public string GetNombre()
-            {
-                return nombre;
-            }
-            public string GetApellido()
-            {
-                return apellido;
-            }
-        }
+        
         static void Main(string[] args)
         {
-            Persona p1 = new Persona("Bob", "Kunga", randomNumber);
+            Person p1 = new Person("Bob", "Kunga");
 
             Console.WriteLine(p1.GetNombre());
             Console.WriteLine(p1.GetApellido());
             Console.WriteLine("Llamando a Lanzar");
-            var randomNumber = new Random().Next(0, 4);
-            p1.Lanzar(randomNumber);
-            Console.WriteLine(p1.GetNombre());
-            Console.WriteLine(p1.GetApellido());
-            Console.WriteLine(p1.Lanzar());
+            Console.WriteLine(p1.Lanzar());        
 
         }
     }
