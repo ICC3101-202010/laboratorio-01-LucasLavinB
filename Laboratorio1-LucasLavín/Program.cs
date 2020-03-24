51657d9d7bc601ed01be8e9330857be28b2879f0
LucasLavin <ljlavin@miuandes.cl>
2020-03-24 15:41:22 -0300

    Laboratorio

--- Laboratorio1-LucasLavín/Program.cs
+++ Laboratorio1-LucasLavín/Program.cs
@@ -4,9 +4,56 @@ namespace Laboratorio1_LucasLavín
 {
     class Program
     {
+        public class Persona
+        {
+            private string nombre;
+            private string apellido;
+            private int lanzar;
+            private string v1;
+            private string v2;
+
+            public Persona(string nombre, string apellido, int lanzar)
+            {
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.lanzar = lanzar;
+
+            }
+
+            public Persona(string v1, string v2)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+            }
+
+            public int Lanzar(int randomNumber)
+            {
+
+                return lanzar;
+            }
+            
+            public string GetNombre()
+            {
+                return nombre;
+            }
+            public string GetApellido()
+            {
+                return apellido;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona p1 = new Persona("Bob", "Kunga", randomNumber);
+
+            Console.WriteLine(p1.GetNombre());
+            Console.WriteLine(p1.GetApellido());
+            Console.WriteLine("Llamando a Lanzar");
+            var randomNumber = new Random().Next(0, 4);
+            p1.Lanzar(randomNumber);
+            Console.WriteLine(p1.GetNombre());
+            Console.WriteLine(p1.GetApellido());
+            Console.WriteLine(p1.Lanzar());
+
         }
     }
 }
